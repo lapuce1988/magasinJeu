7803dde07fb5eb6877c54bd7db8c57d00554f1ef
lapuce1988 <49413546+lapuce1988@users.noreply.github.com>
2020-04-26 16:09:20 +0200

    Update formProductModifier.php

--- views/formProductModifier.php
+++ views/formProductModifier.php
@@ -1,6 +1,6 @@
 <div class="container mt-5">
     <h2 class='pt-3 text-center'>Modifier un article</h2>
-    <form action="index.php?url=validerFormModif" method="post" class="col-md-6 offset-md-3 border border-primary p-3 rounded">
+    <form action="index.php?url=valideFormModifProduct" method="post" class="col-md-6 offset-md-3 border border-primary p-3 rounded">
         <input type="hidden" name="type" id="test" value="update">
         <div class="row">
             <div class="col">
